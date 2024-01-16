6197289446ec6d0dbe0074b66f1f5d4e2046068a
S. Jansen <sa.jansen1@gmail.com>
2024-01-16 22:35:58 +0200

    added top and bottom for traces.

--- frontend/src/logic/generators/gcode.ts
+++ frontend/src/logic/generators/gcode.ts
@@ -4,23 +4,33 @@ import {generateDrillAlignmentFile, generateDrillFile} from "./drills.ts";
 import {getProjectDimensions} from "../processors/project.ts";
 
 export namespace Gcode {
-    function gcodeMoveCommand(location: Location, dimensions: Dimension, config: TracesConfig, linear: boolean = true) {
-        const x = config.offsetX + location.x; // ((dimensions.x + dimensions.width) - (location.x - dimensions.x));
+    type TraceConfig = {
+        dimensions: Dimension
+        mirror: boolean
+        offsetX: number
+        offsetY: number
+        iterations: number
+    }
+
+    function gcodeMoveCommand(location: Location, config: TraceConfig, linear: boolean = true) {
+        const x = config.offsetX + (config.mirror
+            ? ((config.dimensions.x + config.dimensions.width) - (location.x - config.dimensions.x))
+            : location.x);
         const y = config.offsetY + location.y;
         return `G0${linear ? 1 : 0} X${x.toFixed(4)}Y${y.toFixed(4)}`;
     }
 
-    function generateBackAndForthTraces(traces: Trace[], dimensions: Dimension, config: TracesConfig) {
+    function generateBackAndForthTraces(traces: Trace[], config: TraceConfig) {
         return traces
             .map(trace => [
-                gcodeMoveCommand(trace[0], dimensions, config, false),
+                gcodeMoveCommand(trace[0], config, false),
                 `G01 Z0.0000`,
                 ...Array.from(Array(config.iterations))
                     .flatMap((_, iteration) =>
                         trace.map((_, i) => {
                             // Use back and forth motion for the iterations
                             const it = trace[iteration % 2 == 0 ? i : trace.length - i - 1];
-                            return gcodeMoveCommand(it, dimensions, config, true)
+                            return gcodeMoveCommand(it, config, true)
                         })
                     ),
                 `G00 Z2.0000`]
@@ -29,14 +39,14 @@ export namespace Gcode {
             .join("\n\n");
     }
 
-    function generateContinuousTraces(traces: Trace[], dimensions: Dimension, config: TracesConfig) {
+    function generateContinuousTraces(traces: Trace[], config: TraceConfig) {
         return traces
             .map(trace => [
-                gcodeMoveCommand(trace[0], dimensions, config, false),
+                gcodeMoveCommand(trace[0], config, false),
                 `G01 Z0.0000`,
                 ...Array.from(Array(config.iterations))
                     .flatMap(_ =>
-                        trace.map(it => gcodeMoveCommand(it, dimensions, config, true))
+                        trace.map(it => gcodeMoveCommand(it, config, true))
                     ),
                 `G00 Z3.0000`]
                 .join("\n")
@@ -44,8 +54,16 @@ export namespace Gcode {
             .join("\n\n");
     }
 
-    export const generateCopperFile = (project: Project, config: TracesConfig): string => {
+    export const generateCopperFile = (project: Project, side: "top" | "bottom", config: TracesConfig): string => {
         const dimensions = getProjectDimensions(project)
+        const traceConfig: TraceConfig = {
+            dimensions: dimensions,
+            iterations: config.iterations,
+            mirror: side == "bottom",
+            offsetX: config.offsetX,
+            offsetY: config.offsetY
+        }
+
         return [
             "G21",
             "G90",
@@ -55,26 +73,34 @@ export namespace Gcode {
             "M03",
             "G28",
             "G4 P1",
-            generateBackAndForthTraces(project.profile, dimensions, config),
-            generateContinuousTraces(project.traces_top, dimensions, config),
+            generateBackAndForthTraces(project.profile, traceConfig),
+            generateContinuousTraces(side == "top" ? project.traces_top : project.traces_bottom, traceConfig),
             "G00 X0.0000Y0.0000Z3.0000",
             "M300 S2000 P500 ; Beep end",
             "M05",
         ].join("\n");
     }
 
-    export const generateSilkscreenFile = (project: Project, config: TracesConfig): string => {
+    export const generateSilkscreenFile = (project: Project, side: "top" | "bottom", config: TracesConfig): string => {
         const dimensions = getProjectDimensions(project)
+        const traceConfig: TraceConfig = {
+            dimensions: dimensions,
+            iterations: config.iterations,
+            mirror: side == "bottom",
+            offsetX: config.offsetX,
+            offsetY: config.offsetY
+        }
+
         return [
             "G21",
             "G90",
             "G94",
-            "G0 F1400.00",
+            `G0 F${config.feedRate.toFixed(0)}`,
             "G00 Z2.0000",
             "M03",
             "G28",
             "G4 P1",
-            generateBackAndForthTraces(project.silkscreen_top, dimensions, config),
+            generateBackAndForthTraces(side == "top" ? project.silkscreen_top : project.silkscreen_bottom, traceConfig),
             "G00 X0Y0",
             "M300 S2000 P500 ; Beep end",
             "M05",
@@ -83,10 +109,12 @@ export namespace Gcode {
 
     export const generateFiles = (project: Project, config: GcodeConfig) => {
         return {
-            "1_etching.gcode": generateCopperFile(project, config.traces),
-            "2_drill_alignment.gcode": generateDrillAlignmentFile(project, config.drills),
-            "3_silkscreen.gcode": generateSilkscreenFile(project, config.traces),
-            "4_drills.gcode": generateDrillFile(project, config.drills),
+            "1_etching_top.gcode": generateCopperFile(project, "top", config.traces),
+            "1_etching_bottom.gcode": generateCopperFile(project, "bottom", config.traces),
+            "2_drill_alignment_bottom.gcode": generateDrillAlignmentFile(project, config.drills),
+            "3_silkscreen_top.gcode": generateSilkscreenFile(project, "top", config.traces),
+            "3_silkscreen_bottom.gcode": generateSilkscreenFile(project, "bottom", config.traces),
+            "4_drills_top.gcode": generateDrillFile(project, config.drills),
         }
     }
 }
