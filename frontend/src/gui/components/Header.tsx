06e52272c726a039dbd88a46a3105a8751f57050
S. Jansen <samuelanton@hotmail.com>
2024-09-20 10:29:37 +0200

    added loading icons

--- frontend/src/gui/components/Header.tsx
+++ frontend/src/gui/components/Header.tsx
@@ -1,54 +1,58 @@
-import {Component} from "solid-js";
-import {emptyProject, useProject} from "../ProjectContext";
-import {useConfig} from "../ConfigContext";
-import {Persistency} from "../../logic/utils/persistency";
+import { Component, createSignal } from "solid-js";
+import { emptyProject, useProject } from "../ProjectContext";
+import { useConfig } from "../ConfigContext";
+import { Persistency } from "../../logic/utils/persistency";
 import './Header.less';
-import {AiOutlineReload, AiOutlineSave} from "solid-icons/ai";
-import {useChecklist} from "../ChecklistContext";
+import { AiOutlineLoading, AiOutlineReload, AiOutlineSave } from "solid-icons/ai";
+import { useChecklist } from "../ChecklistContext";
 // @ts-ignore
-import {version} from "../../../package.json";
+import { version } from "../../../package.json";
 
 type Props = {
-    onResetProject: () => void
+  onResetProject: () => void
 }
 
 const Header: Component<Props> = (props) => {
-    const {project, loadProject} = useProject();
-    const {config} = useConfig();
-    const {resetChecklist} = useChecklist()
+  const { project, loadProject } = useProject();
+  const { config } = useConfig();
+  const { resetChecklist } = useChecklist()
+  const [isSaving, setIsSaving] = createSignal(false);
 
-    const resetProject = () => {
-        if (!confirm("Are you sure you want to delete this project?")) return;
+  const resetProject = () => {
+    if (!confirm("Are you sure you want to delete this project?")) return;
 
-        loadProject(emptyProject())
+    loadProject(emptyProject())
 
-        if (import.meta.env["PROD"]) {
-            Persistency.saveAll(project, config)
-        } else {
-            console.log("Not saving to localstorage in development")
-        }
-
-        resetChecklist();
-
-        props.onResetProject();
-    };
-
-    const saveProject = () => {
-        Persistency.saveAll(project, config)
+    if (import.meta.env["PROD"]) {
+      Persistency.saveAll(project, config)
+    } else {
+      console.log("Not saving to localstorage in development")
     }
 
-    return <div class={"Header"}>
-        <button onClick={resetProject}
-                title={"Create a new project, but keep config"}>
-            <AiOutlineReload/> New project
-        </button>
-        <button onClick={saveProject}
-                title={"Save project and config"}>
-            <AiOutlineSave/> Save
-        </button>
+    resetChecklist();
 
-        <span class={"version"}>v{version} {import.meta.env["DEV"] ? "(development)" : null}</span>
-    </div>;
+    props.onResetProject();
+  };
+
+  const saveProject = () => {
+    setIsSaving(true);
+    Persistency.saveAll(project, config)
+    setTimeout(() => setIsSaving(false), 200);
+  }
+
+  return <div class={"Header"}>
+    <button onClick={resetProject}
+            title={"Create a new project, but keep config"}>
+      <AiOutlineReload /> New project
+    </button>
+    <button onClick={saveProject}
+            disabled={isSaving()}
+            title={"Save project and config"}>
+      {isSaving() ? <AiOutlineLoading class={"spinner"} /> : <AiOutlineSave />} {isSaving() ? "Saving..." : "Save"}
+    </button>
+
+    <span class={"version"}>v{version} {import.meta.env["DEV"] ? "(development)" : null}</span>
+  </div>;
 }
 
 export default Header;
--- frontend/src/gui/viewer/flatcam/DownloadButton.tsx
+++ frontend/src/gui/viewer/flatcam/DownloadButton.tsx
@@ -1,29 +1,37 @@
-import {Component, createSignal} from "solid-js";
+import { Component, createSignal } from "solid-js";
+import { AiOutlineLoading } from "solid-icons/ai";
+import { runAsync } from "../../../logic/utils/utils";
 
 type Props = {
-    content: () => string,
-    fileName: string,
-    text: string,
+  content: () => string,
+  fileName: string,
+  text: string,
 }
 
 const DownloadButton: Component<Props> = (props) => {
-    const [content, setContent] = createSignal("#");
+  const [content, setContent] = createSignal("#");
+  const [isLoading, setIsLoading] = createSignal(false);
 
-    const onClick = (e: MouseEvent) => {
-        const generatedContent = props.content();
-        if (generatedContent.length == 0) {
-            e.preventDefault();
-            setContent("#");
-        } else {
-            setContent(`data:text/plain;charset=utf-8,${encodeURIComponent(generatedContent)}`)
-        }
-    }
+  const onClick = (e: MouseEvent) => {
+    setIsLoading(true);
+    runAsync(() => {
+      const generatedContent = props.content();
+      setTimeout(() => setIsLoading(false), 100);
 
-    return <a href={content()}
-              onClick={onClick}
-              download={content().length == 0 ? undefined : props.fileName}>
-        {props.text}
-    </a>;
+      if (generatedContent.length == 0) {
+        e.preventDefault();
+        setContent("#");
+      } else {
+        setContent(`data:text/plain;charset=utf-8,${encodeURIComponent(generatedContent)}`)
+      }
+    })
+  }
+
+  return <a href={content()}
+            onClick={onClick}
+            download={content().length == 0 ? undefined : props.fileName}>
+    {isLoading() ? <AiOutlineLoading class={"spinner"} /> : null} {props.text}
+  </a>;
 }
 
 export default DownloadButton;
