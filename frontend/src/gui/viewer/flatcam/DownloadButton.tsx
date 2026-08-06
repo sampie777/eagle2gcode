b8e3cd2a626cfd446fdf5875d59a6f0590b95636
S. Jansen <samuelanton@hotmail.com>
2026-08-06 11:14:47 +0200

    fix: Download button will now download the correct files AFTER being generated (and not before)

--- frontend/src/gui/viewer/flatcam/DownloadButton.tsx
+++ frontend/src/gui/viewer/flatcam/DownloadButton.tsx
@@ -12,7 +12,6 @@ type Props = {
 }
 
 const DownloadButton: Component<Props> = (props) => {
-  const [content, setContent] = createSignal("#");
   const [isLoading, setIsLoading] = createSignal(false);
 
   const onClick = (e: MouseEvent) => {
@@ -20,40 +19,43 @@ const DownloadButton: Component<Props> = (props) => {
 
     const downloadTextareaId = "download-textarea";
     document.getElementById(downloadTextareaId)?.remove();
-    if (e.ctrlKey) e.preventDefault();
 
     runAsync(() => {
       const generatedContent = props.content();
       setTimeout(() => setIsLoading(false), 100);
 
       if (generatedContent.length == 0) {
-        e.preventDefault();
-        setContent("#");
         alert("No GCode generated");
-      } else if (e.ctrlKey) {
-        e.preventDefault();
+        return;
+      }
+
+      if (e.ctrlKey || e.metaKey) {
         const it = document.createElement("textarea");
         it.id = downloadTextareaId;
         it.value = generatedContent;
 
         setTimeout(() => document.body.appendChild(it), 50);
-      } else {
-        setContent(`data:text/plain;charset=utf-8,${encodeURIComponent(generatedContent)}`)
+        return;
       }
+
+      const downloadElement = document.createElement("a");
+      downloadElement.href = `data:text/plain;charset=utf-8,${encodeURIComponent(generatedContent)}`;
+      downloadElement.download = props.fileName;
+      document.body.appendChild(downloadElement);
+      downloadElement.click();
+      document.removeChild(downloadElement);
     })
   }
 
-  return <a href={content()}
-            class={"DownloadButton"}
-            onClick={onClick}
-            download={content().length == 0 ? undefined : props.fileName}
-            title={props.title ?? "Click to download, or Ctrl+Click to view"}>
+  return <button class={"DownloadButton"}
+                 onClick={onClick}
+                 title={props.title ?? "Click to download, or Ctrl+Click to view"}>
     <TbFileDownload />
     <div class={"content"}>
       <span class={"text"}>{isLoading() ? <AiOutlineLoading class={"spinner"} /> : null} {props.text}</span>
       {props.children}
     </div>
-  </a>;
+  </button>;
 }
 
 export default DownloadButton;
